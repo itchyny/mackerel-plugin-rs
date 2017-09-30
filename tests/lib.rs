501d4c95e8c63075d35c6c5aa5b220d64a073376
itchyny <itchyny@hatena.ne.jp>
2017-09-30 20:47:01 +0900

    use to_string to convert str into String

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,19 +2,21 @@ extern crate serde_json;
 
 extern crate mackerel_plugin;
 
+use std::collections::HashMap;
+use std::io::Cursor;
 use mackerel_plugin::*;
 
 #[test]
 fn serialize_graph() {
     let graph = Graph::new(
-        "foo.bar".into(),
-        "Foo bar".into(),
+        "foo.bar".to_string(),
+        "Foo bar".to_string(),
         Unit::Integer,
         vec![
-            Metric::new("foo".into(), "Foo metric".into()),
-            Metric::new("bar".into(), "Bar metric".into()).stacked(),
-            Metric::new("baz".into(), "Baz metric".into()).diff(),
-            Metric::new("qux".into(), "Qux metric".into()).diff().stacked(),
+            Metric::new("foo".to_string(), "Foo metric".to_string()),
+            Metric::new("bar".to_string(), "Bar metric".to_string()).stacked(),
+            Metric::new("baz".to_string(), "Baz metric".to_string()).diff(),
+            Metric::new("qux".to_string(), "Qux metric".to_string()).diff().stacked(),
         ],
     );
     let json: serde_json::Value = serde_json::from_str(
