e5ec55c21363f80979be37b96bd37ee250fe3507
itchyny <itchyny@hatena.ne.jp>
2017-10-02 21:57:42 +0900

    add tests for empty graph name with metric_key_prefix

--- tests/lib.rs
+++ tests/lib.rs
@@ -272,3 +272,65 @@ fn prefix_plugin_output_definitions() {
         })
     );
 }
+
+struct UptimePlugin {}
+
+impl Plugin for UptimePlugin {
+    fn fetch_metrics(&self) -> Result<HashMap<String, f64>, String> {
+        let mut metrics = HashMap::new();
+        metrics.insert("uptime".to_string(), 123456.0);
+        metrics.insert("foobar".to_string(), 456789.0);
+        Ok(metrics)
+    }
+
+    fn graph_definition(&self) -> Vec<Graph> {
+        vec![
+            graph! {
+                name: "",
+                label: "Uptime",
+                unit: "integer",
+                metrics: [
+                    { name: "uptime", label: "uptime" },
+                ]
+            },
+        ]
+    }
+
+    fn metric_key_prefix(&self) -> String {
+        "uptime".to_string()
+    }
+}
+
+#[test]
+fn empty_graph_name_plugin_output_values() {
+    let plugin = UptimePlugin {};
+    let mut out = Cursor::new(Vec::new());
+    let now = current_epoch();
+    assert_eq!(plugin.output_values(&mut out).is_ok(), true);
+    let out_str = String::from_utf8(out.into_inner()).unwrap();
+    assert_eq!(out_str.contains(&format!("{}\t{}\t{}\n", "uptime.uptime", 123456.0, now)), true);
+    assert_eq!(out_str.contains("uptime.foobar"), false);
+}
+
+#[test]
+fn empty_graph_name_plugin_output_definitions() {
+    let plugin = UptimePlugin {};
+    let mut out = Cursor::new(Vec::new());
+    assert_eq!(plugin.output_definitions(&mut out).is_ok(), true);
+    let out_str = String::from_utf8(out.into_inner()).unwrap();
+    assert_eq!(out_str.starts_with("# mackerel-agent-plugin\n"), true);
+    assert_eq!(
+        serde_json::from_str::<serde_json::Value>(out_str.chars().skip(24).collect::<String>().as_ref()).unwrap(),
+        json!({
+            "graphs": {
+                "uptime": {
+                    "label": "Uptime",
+                    "metrics": [
+                        { "name": "uptime", "label": "uptime", "stacked": false },
+                    ],
+                    "unit": "integer",
+                },
+            }
+        })
+    );
+}
